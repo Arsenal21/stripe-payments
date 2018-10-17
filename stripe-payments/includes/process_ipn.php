ad436fb662ded53acc441fb5202efab6c7cf7575
erommel <eminemwalsh@gmail.com>
2018-10-17 20:53:12 +0300

    Fixed action name from previous commit d3c9fe3b280af72594e733eff4f945c5892f266c

--- stripe-payments/includes/process_ipn.php
+++ stripe-payments/includes/process_ipn.php
@@ -603,7 +603,7 @@ if ( isset( $opt[ 'send_emails_to_seller' ] ) ) {
 
 	$subj	 = apply_filters( 'asp_seller_email_subject', $subj, $post_data );
 	$body	 = apply_filters( 'asp_seller_email_body', $body, $post_data );
-	$headers = apply_filters( 'asp_buyer_email_headers', $headers, $post_data );
+	$headers = apply_filters( 'asp_seller_email_headers', $headers, $post_data );
 
 	wp_mail( $to, $subj, $body, $headers );
 	ASP_Debug_Logger::log( 'Notification email sent to seller: ' . $to . ', From email address used: ' . $from );
