6b4d976eebab803e9b4b1ad751f5644bd1c6360f
erommel <eminemwalsh@gmail.com>
2018-11-15 16:06:11 +0200

    Removed some classes and action hooks from autolading on plugin load. Those are only loaded when needed.

--- stripe-payments/accept-stripe-payments.php
+++ stripe-payments/accept-stripe-payments.php
@@ -46,7 +46,6 @@ $ASPMain = new ASPMain();
  * ---------------------------------------------------------------------------- */
 require_once( WP_ASP_PLUGIN_PATH . 'includes/class-debug-logger.php' );
 require_once( WP_ASP_PLUGIN_PATH . 'public/class-asp.php' );
-require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-asp.php' );
 require_once( WP_ASP_PLUGIN_PATH . 'admin/includes/class-products.php' );
 require_once( WP_ASP_PLUGIN_PATH . 'admin/includes/class-coupons.php' );
 require_once( WP_ASP_PLUGIN_PATH . 'admin/includes/class-order.php' );
@@ -64,7 +63,6 @@ register_deactivation_hook( __FILE__, array( 'AcceptStripePayments', 'deactivate
 /*
  */
 add_action( 'plugins_loaded', array( 'AcceptStripePayments', 'get_instance' ) );
-add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcode', 'get_instance' ) );
 
 /* ----------------------------------------------------------------------------*
  * Dashboard and Administrative Functionality
@@ -81,24 +79,25 @@ add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcode', 'get_insta
  * The code below is intended to to give the lightest footprint possible.
  */
 if ( is_admin() ) {
+    //check and redirect old Settings page
+    add_action( 'init', 'asp_init_handler' );
 
     require_once( plugin_dir_path( __FILE__ ) . 'admin/class-asp-admin.php' );
     add_action( 'plugins_loaded', array( 'AcceptStripePayments_Admin', 'get_instance' ) );
+    add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'asp_stripe_add_settings_link' );
+} else {
+    require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-asp.php' );
+    add_filter( 'the_content', 'asp_filter_post_type_content' );
+    add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcode', 'get_instance' ) );
 }
 
 /* Add a link to the settings page in the plugins listing page */
 
-function asp_stripe_add_settings_link( $links, $file ) {
-    if ( $file == plugin_basename( __FILE__ ) ) {
-	$settings_link = '<a href="edit.php?post_type=stripe_order&page=stripe-payments-settings">Settings</a>';
-	array_unshift( $links, $settings_link );
-    }
+function asp_stripe_add_settings_link( $links ) {
+    $settings_link = '<a href="edit.php?post_type=stripe_order&page=stripe-payments-settings">' . __( 'Settings', 'stripe-payments' ) . '</a>';
+    array_unshift( $links, $settings_link );
     return $links;
 }
-
-add_filter( 'plugin_action_links', 'asp_stripe_add_settings_link', 10, 2 );
-//check and redirect old Settings page
-add_action( 'init', 'asp_init_handler' );
 
 register_activation_hook( __FILE__, 'asp_activation_hook_handler' );
 
@@ -140,8 +139,6 @@ function asp_init_handler() {
 	require_once(WP_ASP_PLUGIN_PATH . 'admin/includes/class-products-meta-boxes.php');
     }
 }
-
-add_filter( 'the_content', 'asp_filter_post_type_content' );
 
 function asp_filter_post_type_content( $content ) {
     global $post;
--- stripe-payments/admin/class-asp-admin.php
+++ stripe-payments/admin/class-asp-admin.php
@@ -38,10 +38,6 @@ class AcceptStripePayments_Admin {
 	// Add the options page and menu item.
 	add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
 
-	// Add an action link pointing to the options page.
-	$plugin_basename = plugin_basename( plugin_dir_path( realpath( dirname( __FILE__ ) ) ) . $this->plugin_slug . '.php' );
-	add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
-
 	//Enqueue required scripts and styles
 	add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
@@ -822,20 +818,6 @@ class AcceptStripePayments_Admin {
 
     public function display_addons_menu_page() {
 	include_once( 'views/addons-listing.php' );
-    }
-
-    /**
-     * Add settings action link to the plugins page.
-     *
-     * @since    1.0.0
-     */
-    public function add_action_links( $links ) {
-
-	return array_merge(
-	array(
-	    'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', 'stripe-payments' ) . '</a>'
-	), $links
-	);
     }
 
     static function get_email_tags_descr() {
