626b8db9f150d3d0d738511f86dfc6ab4a28809a
Alexander C <eminemwalsh@gmail.com>
2019-06-10 15:09:27 +0300

    Updated main plugin file for compatability between master and ng branches.

--- stripe-payments/accept-stripe-payments.php
+++ stripe-payments/accept-stripe-payments.php
@@ -3,7 +3,6 @@
 /**
  * Plugin Name: Stripe Payments
  * Description: Easily accept credit card payments via Stripe payment gateway in WordPress.
-
  * Version: 1.9.24
  * Author: Tips and Tricks HQ, wptipsntricks
  * Author URI: https://www.tipsandtricks-hq.com/
@@ -44,17 +43,12 @@ class ASPMain {
 	register_activation_hook( __FILE__, array( 'AcceptStripePayments', 'activate' ) );
 	register_deactivation_hook( __FILE__, array( 'AcceptStripePayments', 'deactivate' ) );
 
-add_action( 'plugins_loaded', array( 'AcceptStripePayments', 'get_instance' ) );
-require_once(plugin_dir_path( __FILE__ ) . 'includes/session-handler-class.php');
-require_once( WP_ASP_PLUGIN_PATH . 'includes/item-class.php' );
-require_once( WP_ASP_PLUGIN_PATH . 'includes/class-asp-ng-payment-handler.php' );
+	add_action( 'plugins_loaded', array( 'AcceptStripePayments', 'get_instance' ) );
 
-    require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-asp.php' );
-    require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-ng.php' );
-    add_filter( 'the_content', 'asp_filter_post_type_content' );
-    add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcode', 'get_instance' ) );
-    add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcodeNG', 'get_instance' ) );
-}
+	if ( is_admin() ) {
+	    require_once( WP_ASP_PLUGIN_PATH . 'admin/class-asp-admin.php' );
+	    add_action( 'plugins_loaded', array( 'AcceptStripePayments_Admin', 'get_instance' ) );
+	}
 
 	require_once( WP_ASP_PLUGIN_PATH . 'includes/session-handler-class.php');
 	require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-asp.php' );
@@ -66,6 +60,13 @@ require_once( WP_ASP_PLUGIN_PATH . 'includes/class-asp-ng-payment-handler.php' )
 	add_action( 'init', array( $ASPProducts, 'register_post_type' ), 0 );
 	$ASPOrder	 = ASPOrder::get_instance();
 	add_action( 'init', array( $ASPOrder, 'register_post_type' ), 0 );
+
+	//NG-related
+	require_once( WP_ASP_PLUGIN_PATH . 'includes/item-class.php' );
+	require_once( WP_ASP_PLUGIN_PATH . 'includes/class-asp-ng-payment-handler.php' );
+	require_once( WP_ASP_PLUGIN_PATH . 'public/includes/class-shortcode-ng.php' );
+	add_action( 'plugins_loaded', array( 'AcceptStripePaymentsShortcodeNG', 'get_instance' ) );
+	//NG-related end
     }
 
     static function load_stripe_lib() {
