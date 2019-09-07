640a54f8b53dd980b6108f98f377044c05e38cd8
erommel <eminemwalsh@gmail.com>
2019-09-07 15:01:19 +0300

    Added "New API only" hint for Payment Popup Button Text option

--- stripe-payments/admin/class-asp-admin.php
+++ stripe-payments/admin/class-asp-admin.php
@@ -362,7 +362,7 @@ function show_admin_notices() {
 	add_settings_field( 'currency_symbol', __( 'Currency Symbol', 'stripe-payments' ), array( &$this, 'settings_field_callback' ), $this->plugin_slug, 'AcceptStripePayments-global-section', array( 'field' => 'currency_symbol', 'desc' => '', 'size' => 10 ) );
 	add_settings_field( 'button_text', __( 'Button Text', 'stripe-payments' ), array( &$this, 'settings_field_callback' ), $this->plugin_slug, 'AcceptStripePayments-global-section', array( 'field'	 => 'button_text',
 	    'desc'	 => __( 'Example: Buy Now, Pay Now etc.', 'stripe-payments' ) ) );
-	add_settings_field( 'popup_button_text', __( 'Payment Popup Button Text', 'stripe-payments' ), array( &$this, 'settings_field_callback' ), $this->plugin_slug, 'AcceptStripePayments-global-section', array( 'field'	 => 'popup_button_text',
+	add_settings_field( 'popup_button_text', __( 'Payment Popup Button Text', 'stripe-payments' ).$new_api_str, array( &$this, 'settings_field_callback' ), $this->plugin_slug, 'AcceptStripePayments-global-section', array( 'field'	 => 'popup_button_text',
 	    'desc'	 => __( '%s is replaced by formatted payment amount. If empty, it defaults to "Pay %s"', 'stripe-payments' ) ) );
 	add_settings_field( 'dont_save_card', __( 'Do Not Save Card Data on Stripe', 'stripe-payments' ), array( &$this, 'settings_field_callback' ), $this->plugin_slug, 'AcceptStripePayments-global-section', array( 'field'	 => 'dont_save_card',
 	    'desc'	 => __( 'When this checkbox is checked, the transaction won\'t create the customer (no card will be saved for that).', 'stripe-payments' ) ) );
