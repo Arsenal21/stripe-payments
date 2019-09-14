ce4ce29b7cab620a9e43f1d861b32cddc386a05c
erommel <eminemwalsh@gmail.com>
2019-09-14 14:51:04 +0300

    Updated build.sh script to remove excess data from resulting archive

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 stable=$(grep "Stable tag:" ./stripe-payments/readme.txt)
 version=$(echo $stable | sed 's/Stable tag://' | xargs)
 rm -f ./stripe-payments_$version.zip
-zip -9 -r ./stripe-payments_$version.zip ./stripe-payments/ -x *_debug_log.txt
+zip -r -J -X ./stripe-payments_$version.zip ./stripe-payments/ -x *_debug_log.txt
