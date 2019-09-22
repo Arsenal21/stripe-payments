7eaa102ef4c3cfe75489592b8920c9c26f992803
erommel <eminemwalsh@gmail.com>
2019-09-22 16:24:23 +0300

    Added css and js minify\combine for payment popup scripts into build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#combine css
+cat ./stripe-payments/public/views/templates/default/pure.css ./stripe-payments/public/views/templates/default/pp-style.css | cleancss -o ./stripe-payments/public/views/templates/default/pp-combined.min.css --s0
+
 stable=$(grep "Stable tag:" ./stripe-payments/readme.txt)
 version=$(echo $stable | sed 's/Stable tag://' | xargs)
 rm -f ./stripe-payments_$version.zip
