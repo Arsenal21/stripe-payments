ca4409aa54fe07cb7c0e6998be2271bf53e470f4
erommel <eminemwalsh@gmail.com>
2019-09-22 16:27:21 +0300

    Added pp-handler.js minify to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#combine css
+#minify and combine css
 cat ./stripe-payments/public/views/templates/default/pure.css ./stripe-payments/public/views/templates/default/pp-style.css | cleancss -o ./stripe-payments/public/views/templates/default/pp-combined.min.css --s0
+
+#minify js
+uglifyjs -c -m -o ./stripe-payments/public/assets/js/pp-handler.min.js ./stripe-payments/public/assets/js/pp-handler.js
 
 stable=$(grep "Stable tag:" ./stripe-payments/readme.txt)
 version=$(echo $stable | sed 's/Stable tag://' | xargs)
