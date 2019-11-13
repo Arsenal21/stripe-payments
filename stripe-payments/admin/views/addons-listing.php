e4f0e2309d412663e11a95df61af96c003a2671b
amin.alienware.a51 <amin0@DESKTOP-KUST9TJ.Home>
2019-11-13 14:47:54 +1100

    added the iDEAL gateway to the addons list.

--- stripe-payments/admin/views/addons-listing.php
+++ stripe-payments/admin/views/addons-listing.php
@@ -149,6 +149,16 @@ $output = '';
 		array_push( $addons_data, $addon_13 );
 
 		$addon_14 = array(
+			'name'         => __( 'iDEAL Payments Addon', 'stripe-payments' ),
+			'thumbnail'    => WP_ASP_PLUGIN_URL . '/admin/assets/images/iDEAL-payment-gateway-addon.png',
+			'description'  => __( 'The iDEAL Addon can be used along side the Stripe Payments Plugin to allow your customers to pay using iDEAL gateway.', 'stripe-payments' ),
+			'page_url'     => 'https://s-plugins.com/stripe-ideal-payment-addon/',
+			'settings_url' => 'edit.php?post_type='.ASPMain::$products_slug.'&page=stripe-payments-settings#ideal',
+			'installed'    => class_exists( 'ASPACF_main' ) ? true : false,
+		);
+		array_push( $addons_data, $addon_14 );
+                
+		$addon_15 = array(
 			'name'         => __( 'Additional Custom Fields', 'stripe-payments' ),
 			'thumbnail'    => WP_ASP_PLUGIN_URL . '/admin/assets/images/additional-custom-fields.png',
 			'description'  => __( 'The Additional Custom Fields Addon allows you to collect information from your customers before they proceed to checkout.', 'stripe-payments' ),
@@ -156,7 +166,7 @@ $output = '';
 			'settings_url' => 'edit.php?post_type='.ASPMain::$products_slug.'&page=stripe-payments-settings#acf',
 			'installed'    => class_exists( 'ASPACF_main' ) ? true : false,
 		);
-		array_push( $addons_data, $addon_14 );
+		array_push( $addons_data, $addon_15 );
 
 		/* Show the addons list */
 		foreach ( $addons_data as $addon ) {
