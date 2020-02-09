Arsenal21/stripe-payments

commit 081b68688a2b8d9d34e5c5dae48f5cf0ea60676a

Payment process is not started when CC details are incomplete

stripe-payments/public/assets/js/pp-handler.js
@@ -263,6 +263,7 @@ card.addEventListener('change', function (event) {
 	} else {
 		cardErrorCont.textContent = '';
 	}
+	vars.data.cardComplete = event.complete;
 });
 
 submitBtn.addEventListener('click', function () {
@@ -275,6 +276,12 @@ form.addEventListener('submit', function (event) {
 	event.preventDefault();
 
 	if (!canProceed()) {
+		return false;
+	}
+
+	if (('def' === vars.data.currentPM || !vars.data.currentPM) && !vars.data.cardComplete) {
+		event.preventDefault();
+		card.focus();
 		return false;
 	}
 

commit 618cf933e0adc9e7e748c359b54392e424bfbc37

Fixed all-off coupons were broken in 081b68688a2b8d9d34e5c5dae48f5cf0ea60676a

stripe-payments/public/assets/js/pp-handler.js
@@ -279,7 +279,7 @@ form.addEventListener('submit', function (event) {
 		return false;
 	}
 
-	if (('def' === vars.data.currentPM || !vars.data.currentPM) && !vars.data.cardComplete) {
+	if (!is_full_discount() && ('def' === vars.data.currentPM || !vars.data.currentPM) && !vars.data.cardComplete) {
 		event.preventDefault();
 		card.focus();
 		return false;