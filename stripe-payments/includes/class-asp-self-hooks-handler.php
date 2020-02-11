bba9eedb859fbf402ee391a65129ef53dd11bc15
erommel <eminemwalsh@gmail.com>
2020-02-11 16:03:21 +0200

    eMember: fixed user name was never passed to the plugin eMember: made integration work when using new Stripe API eMember: integration now works with SOFORT, AliPay and iDEAL payment methods

--- stripe-payments/includes/class-asp-self-hooks-handler.php
+++ stripe-payments/includes/class-asp-self-hooks-handler.php
@@ -7,6 +7,15 @@ class ASP_Self_Hooks_Handler {
 	public function __construct() {
 		$this->main = AcceptStripePayments::get_instance();
 		add_action( 'asp_ng_product_mode_keys', array( $this, 'ng_product_mode_keys_handler' ) );
+
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ), 0 );
+	}
+
+	public function plugins_loaded() {
+		//WP eMember integration
+		if ( function_exists( 'wp_emember_install' ) ) {
+			add_action( 'asp_stripe_payment_completed', array( $this, 'handle_eMember_signup' ), 10, 2 );
+		}
 	}
 
 	public function ng_product_mode_keys_handler( $product_id ) {
@@ -42,6 +51,81 @@ class ASP_Self_Hooks_Handler {
 		$this->main->is_live = false;
 		$this->APIPubKey     = $this->main->get_setting( 'api_publishable_key_test' );
 		$this->APISecKey     = $this->main->get_setting( 'api_secret_key_test' );
+	}
+
+	public function handle_emember_signup( $data, $charge ) {
+
+		if ( empty( $data['product_id'] ) ) {
+			return;
+		}
+
+		//let's check if Membership Level is set for this product
+		$level_id = get_post_meta( $data['product_id'], 'asp_product_emember_level', true );
+		if ( empty( $level_id ) ) {
+			return;
+		}
+
+		//let's form data required for eMember_handle_subsc_signup_stand_alone function and call it
+		$first_name = '';
+		$last_name  = '';
+		if ( ! empty( $data['customer_name'] ) ) {
+			// let's try to create first name and last name from full name
+			$parts      = explode( ' ', $data['customer_name'] );
+			$last_name  = array_pop( $parts );
+			$first_name = implode( ' ', $parts );
+		}
+		$addr_street  = isset( $_POST['stripeBillingAddressLine1'] ) ? $_POST['stripeBillingAddressLine1'] : '';
+		$addr_zip     = isset( $_POST['stripeBillingAddressZip'] ) ? $_POST['stripeBillingAddressZip'] : '';
+		$addr_city    = isset( $_POST['stripeBillingAddressCity'] ) ? $_POST['stripeBillingAddressCity'] : '';
+		$addr_state   = isset( $_POST['stripeBillingAddressState'] ) ? $_POST['stripeBillingAddressState'] : '';
+		$addr_country = isset( $_POST['stripeBillingAddressCountry'] ) ? $_POST['stripeBillingAddressCountry'] : '';
+
+		if ( empty( $addr_street ) && ! empty( $charge->source->address_line1 ) ) {
+			$addr_street = $charge->source->address_line1;
+		}
+
+		if ( empty( $addr_zip ) && ! empty( $charge->source->address_zip ) ) {
+			$addr_zip = $charge->source->address_zip;
+		}
+
+		if ( empty( $addr_city ) && ! empty( $charge->source->address_city ) ) {
+			$addr_city = $charge->source->address_city;
+		}
+
+		if ( empty( $addr_state ) && ! empty( $charge->source->address_state ) ) {
+			$addr_state = $charge->source->address_state;
+		}
+
+		if ( empty( $addr_country ) && ! empty( $charge->source->address_country ) ) {
+			$addr_country = $charge->source->address_country;
+		}
+
+		$ipn_data = array(
+			'payer_email'     => $data['stripeEmail'],
+			'first_name'      => $first_name,
+			'last_name'       => $last_name,
+			'txn_id'          => $data['txn_id'],
+			'address_street'  => $addr_street,
+			'address_city'    => $addr_city,
+			'address_state'   => $addr_state,
+			'address_zip'     => $addr_zip,
+			'address_country' => $addr_country,
+		);
+
+		ASP_Debug_Logger::log( 'Calling eMember_handle_subsc_signup_stand_alone' );
+
+		$emember_id = '';
+		if ( class_exists( 'Emember_Auth' ) ) {
+			//Check if the user is logged in as a member.
+			$emember_auth = Emember_Auth::getInstance();
+			$emember_id   = $emember_auth->getUserInfo( 'member_id' );
+		}
+
+		if ( defined( 'WP_EMEMBER_PATH' ) ) {
+			require_once WP_EMEMBER_PATH . 'ipn/eMember_handle_subsc_ipn_stand_alone.php';
+			eMember_handle_subsc_signup_stand_alone( $ipn_data, $level_id, $data['txn_id'], $emember_id );
+		}
+
 	}
 
 }
